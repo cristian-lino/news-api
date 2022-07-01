d416c360b2503fbb6014cfeb7720cd32eaf970e4
Gabriel Loureiro <gabrieloureirof@gmail.com>
2022-07-01 12:17:39 -0300

    fix: fix jwt response on auth method

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const port = 5000
+const port = 5001
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,43 +1,44 @@
-const auth =  require('../services/auth')
-const authConfig = require('../config/auth');
-const jwt = require('jsonwebtoken');
+const auth = require("../services/auth");
+const authConfig = require("../config/auth");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/User');
-const GeneralPermission = require('../models/GeneralPermission');
-const SystemRole = require('../models/SystemRole');
+const User = require("../models/User");
+const SystemRole = require("../models/SystemRole");
 
 const create = async (req, res) => {
-    try {
-        const { email, password } = req.body
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ where: { email: email } })
+    const user = await User.findOne({ where: { email: email } });
 
-        if (!user) {
-            return res.status(401).json({error: "User invalid."})
-        }
-
-        if (!auth.checkPassword(user, password)) {
-            return res.status(401).json({error: "Password invalid."})
-        }
-
-        const { id, name } = user
-        const role = await SystemRole.findByPk(user.role_id)
-        const p = await GeneralPermission.findAll()
-        const permissions = p.filter(a => a.role_id === user.role_id)
-
-        return res.json({
-            id,
-            email,
-            name,
-            role,
-            permissions,
-            token: jwt.sign({id}, authConfig.secret, {
-                expiresIn: authConfig.expiresIn
-            })
-        })
-    } catch (error) {
-        throw error
+    if (!user) {
+      return res.status(401).json({ error: "User invalid." });
     }
-}
 
-module.exports = { create }
+    if (!auth.checkPassword(user, password)) {
+      return res.status(401).json({ error: "Password invalid." });
+    }
+
+    const { id, name } = user;
+    const role = await SystemRole.findByPk(user.role_id);
+
+    const returnedRole = {
+      id: role.id,
+      name: role.name,
+    };
+
+    return res.json({
+      token: jwt.sign(
+        { id, email, name, role: returnedRole },
+        authConfig.secret,
+        {
+          expiresIn: authConfig.expiresIn,
+        }
+      ),
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { create };

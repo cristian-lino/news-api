4fe310f169b435d3bc17768b15b7810af27c05ac
Gabriel Loureiro <gabrieloureirof@gmail.com>
2022-07-01 15:38:20 -0300

    feat: change populateDB

--- src/db.js
+++ src/db.js
@@ -1,117 +1,62 @@
-const database = require('./database/index');
-const auth =  require('./services/auth')
+const database = require("./database/index");
+const auth = require("./services/auth");
 
-const User = require('./models/User');
-const SystemRole = require('./models/SystemRole');
-const GeneralPermission = require('./models/GeneralPermission');
-const Channel = require('./models/Channel');
-const ChannelUserLikes = require('./models/ChannelUserLikes');
-const News = require('./models/News');
+const User = require("./models/User");
+const SystemRole = require("./models/SystemRole");
 
-const syncdb = async () => {
-    try {
-        const res = await database.sync();
-    } catch (error) {
-        console.log(error);
-    }
+const syncDb = async () => {
+  try {
+    await database.sync();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const createSomeData = async () => {
-    try {
-        const systemRole1 = await SystemRole.create({
-            id: 1,
-            name: 'Administrador'
-        })
-        const systemRole2 = await SystemRole.create({
-            id: 2,
-            name: 'Usuário comum'
-        })
-        const systemRole3 = await SystemRole.create({
-            id: 3,
-            name: 'Criador de conteúdo'
-        })
-        
-    
-        const user = await User.create({
-            id: 1,
-            name: 'Admin',
-            email: 'admin@admin.com',
-            password: auth.createPasswordHash('admin'),
-            role_id: 1
-        })
-        
-        const homePermission1 = await GeneralPermission.create({
-            id: 1,
-            screen: 'HOME',
-            allow_read: true,
-            allow_create: true,
-            allow_edit: true,
-            allow_delete: true,
-            role_id: 1
-        })
-        const homePermission2 = await GeneralPermission.create({
-            id: 2,
-            screen: 'HOME',
-            allow_read: true,
-            allow_create: false,
-            allow_edit: false,
-            allow_delete: false,
-            role_id: 2
-        })
-        const homePermission3 = await GeneralPermission.create({
-            id: 3,
-            screen: 'HOME',
-            allow_read: true,
-            allow_create: true,
-            allow_edit: false,
-            allow_delete: false,
-            role_id: 3
-        })
-        
-        const channelPermission1 = await GeneralPermission.create({
-            id: 4,
-            screen: 'CHANNEL',
-            allow_read: true,
-            allow_create: true,
-            allow_edit: true,
-            allow_delete: true,
-            role_id: 1
-        })
-        const channelPermission2 = await GeneralPermission.create({
-            id: 5,
-            screen: 'CHANNEL',
-            allow_read: true,
-            allow_create: false,
-            allow_edit: false,
-            allow_delete: false,
-            role_id: 2
-        })
-        const channelPermission3 = await GeneralPermission.create({
-            id: 6,
-            screen: 'CHANNEL',
-            allow_read: true,
-            allow_create: true,
-            allow_edit: false,
-            allow_delete: false,
-            role_id: 3
-        })
-        const generalPermission5 = await GeneralPermission.create({
-            id: 7,
-            screen: 'USER_MANAGEMENT',
-            allow_read: true,
-            allow_create: true,
-            allow_edit: true,
-            allow_delete: true,
-            role_id: 1
-        })
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    await Promise.all([
+      SystemRole.create({
+        id: 1,
+        name: "admin",
+      }),
+      SystemRole.create({
+        id: 2,
+        name: "creator",
+      }),
+      SystemRole.create({
+        id: 3,
+        name: "consumer",
+      }),
+      User.create({
+        id: 1,
+        name: "Administrador",
+        email: "admin@admin.com",
+        password: auth.createPasswordHash("admin"),
+        role_id: 1,
+      }),
+      User.create({
+        id: 2,
+        name: "Criador de conteúdo",
+        email: "creator@creator.com",
+        password: auth.createPasswordHash("creator"),
+        role_id: 2,
+      }),
+      User.create({
+        id: 3,
+        name: "Usuário",
+        email: "user@user.com",
+        password: auth.createPasswordHash("user"),
+        role_id: 3,
+      }),
+    ]);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const init = async () => {
-    await syncdb()
-    await createSomeData()
-}
+  await syncDb();
+  await createSomeData();
+};
 
-init()
+init();
